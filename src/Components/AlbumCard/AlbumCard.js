85f919f66ad66dde2ec6f73867c98d841062a8c7
Max Bregman <67295227+Max9545@users.noreply.github.com>
2021-05-15 16:29:08 -0600

    Make display genres method

--- src/Components/AlbumCard/AlbumCard.js
+++ src/Components/AlbumCard/AlbumCard.js
@@ -3,7 +3,9 @@ import './AlbumCard.css'
 function AlbumCard({ album }) {
 
 
-
+  const displayGenres = (list) => {
+   return list.map(genre => <p>{genre}</p>)
+  }
 
 
   return (
@@ -11,6 +13,8 @@ function AlbumCard({ album }) {
       <img src={album.coverImage}/>
       <h2>{album.artists.name}</h2>
       <h3>{album.title}</h3>
+      <p>{album.year}</p>
+      {displayGenres(album.genres)}
     </>
   )
 }
