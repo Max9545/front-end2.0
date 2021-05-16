8cdd51b535a1d153e3d5e23890687f9817b4ee94
Marika <marikashanahan@gmail.com>
2021-05-16 12:32:32 -0600

    Import BrowserRouter and apply to index.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import AlbumCard from '../AlbumCard/AlbumCard';
 import AlbumCardsDisplay from '../AlbumCardsDisplay/AlbumCardsDisplay'
 import Navigation from './Navigation';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
 
 
@@ -10,7 +11,17 @@ function App() {
       <header>Future Header / Branding mark</header>
       <Navigation />
       <main>
-        <p>Card Container</p>
+        <Switch>
+          <Route exact path="/">
+            <p>Card Container</p>
+          </Route>
+          <Route path="/liked">
+            <p>Eventual liked album cards Container</p>
+          </Route>
+          <Route path="">
+            <h1>This page doesn't exist! Please navigate back to home with the sidebar</h1>
+          </Route>
+        </Switch>
       </main>
     </div>
   );
