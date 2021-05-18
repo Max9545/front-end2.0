1f7d1eed30610c93f35f4261ddc39f7ffa8a3224
Reggie Thompson <reggie.thompson4@gmail.com>
2021-05-18 14:46:59 -0600

    Refactor intercept

--- cypress/integration/details-test.js
+++ cypress/integration/details-test.js
@@ -2,14 +2,18 @@ import { cyan } from "@material-ui/core/colors"
 
 describe('', () => {
   beforeEach(() => {
-    cy.intercept('https://pure-hollows-05817.herokuapp.com/https://tranquil-depths-91575.herokuapp.com/graphql', 
-      {fixture: 'details-fixture.json'})
-    cy.visit('http://localhost:3000/')
-  });
+    cy
+    .intercept('POST', 'https://pure-hollows-05817.herokuapp.com/https://tranquil-depths-91575.herokuapp.com/graphql', (req) => {
+      if (req.body.query.includes('The Payback')) {
+        req.reply({ fixture: 'details-fixture.json' })
+      }
+    })
+    .visit('http://localhost:3000')
+  })
 
-  it.only('should display the current album cover', () => {
+  it('should display the current album cover', () => {
     cy.get('[data-cy=album-cover')
-      .should('exist')
+      .should('have.attr', 'src', 'https://img.discogs.com/MUELn9ObTL-ZpxyUgF5M9D_Kumc=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-719877-1247870750.jpeg.jpg')
   });
 
   it('should allow users to add the current album to their collection of favorites', () => {
@@ -24,22 +28,23 @@ describe('', () => {
 
   it('should display the current album\'s artsit', () => {
     cy.get('[data-cy=artist-name')
-      .should('eq', 'James Brown')
+      .contains('James Brown')
   });
 
   it('should display the current album title', () => {
     cy.get('[data-cy=album-title')
-      .should('eq', 'The Payback')
+      .contains('The Payback')
   });
 
   it('should display the year the current album was released', () => {
     cy.get('[data-cy=release-year')
-      .should('eq', 'Released: 1973')
+      .contains('Released: 1973')
   });
 
   it('should display a playlist where users can preview that album', () => {
     cy.get('[data-cy=web-player')
-      .should('exist')
+      .should('have.attr', 'src')
+      .should('include', 'https://open.spotify.com/embed/album/')
   });
 
   it('should allow users to close the modal window', () => {
