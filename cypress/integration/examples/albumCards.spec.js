1c9d4975000b06e5cf6f6c97de28d197fcc22c90
Max Bregman <67295227+Max9545@users.noreply.github.com>
2021-05-17 18:44:15 -0600

    Make cover test with 'have.attr' of 'src' with value of img source

--- cypress/integration/examples/albumCards.spec.js
+++ cypress/integration/examples/albumCards.spec.js
@@ -5,12 +5,19 @@ context('Album Cards', () => {
     cy.visit('http://localhost:3000')
   })
 
+  it.only('Should have an album cover', () => {
+    cy
+    .get('[data-cy=cover]').should('exist')
+    .should('have.attr', 'src', 'https://img.discogs.com/lcaNDTtBOojvbG2a5Fo8xdfzFXI=/fit-in/600x606/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-538636-1435321700-7614.jpeg.jpg')
+  })
+
   it('Should have an album title', () => {
-    cy.get('[data-cy=card]').should('exist')
+    cy
+    .get('[data-cy=card]').should('exist')
     .get('[data-cy=title]').should('exist').should('contain', 'Freak Out')
   })
 
-  it.only('Should have album genres', () => {
+  it('Should have album genres', () => {
     cy
     .get('[data-cy=genre]').should('exist')
     .should('contain', 'Electronic')
