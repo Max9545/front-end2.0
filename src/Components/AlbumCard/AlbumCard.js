a241647838a24ef17fa019634333abbb55142eec
Marika <marikashanahan@gmail.com>
2021-05-19 10:44:55 -0600

    Create and apply artist name helper function

--- src/Components/AlbumCard/AlbumCard.js
+++ src/Components/AlbumCard/AlbumCard.js
@@ -1,6 +1,7 @@
 import { GET_SINGLE_ALBUM }from '../../queries';
 import { useQuery } from '@apollo/client';
 import './AlbumCard.css'
+import { ArtTrackOutlined } from '@material-ui/icons';
 
 function AlbumCard({ title }) {
   const { loading, error, data } = useQuery(GET_SINGLE_ALBUM, {
@@ -14,17 +15,24 @@ function AlbumCard({ title }) {
    return list.map(genre => <p className='genre' data-cy='genre'>{genre}</p>)
   }
 
+  const displayArtists = (list) => {
+    return list.map(name => <p>{ name }</p>)
+   }
+
   const deconstruct = ({ coverImage, title, year, artists, genres }) => {
     return (
       <>
-      <img className='cover' data-cy='cover'src={data.coverImage}/>
-      <h2 className='title' data-cy='title'>{title}</h2>
-      <h3 className='artist' data-cy='artist'>{name}</h3>
-      <p className='date' data-cy='date'>This Edition Released in {year}</p>
-      <div className='genre-container' data-cy='genre-container'>
-        {displayGenres(genres)}
-      </div>
-      <button className='discogs-link' data-cy='discogs-link' >Purchase on Discogs</button>
+        <img className='cover' data-cy='cover'src={coverImage}/>
+        <h2 className='title' data-cy='title'>{title}</h2>
+        <div className='artist-container' data-cy='artist-container'>
+          {displayArtists(artists)}
+        </div>
+        <h3 className='artist' data-cy='artist'>{}</h3>
+        <p className='date' data-cy='date'>This Edition Released in {year}</p>
+        <div className='genre-container' data-cy='genre-container'>
+          {displayGenres(genres)}
+        </div>
+        <button className='discogs-link' data-cy='discogs-link' >Purchase on Discogs</button>
       </>
     )
   }
