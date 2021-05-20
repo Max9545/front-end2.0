d00efaecc67354c7f93543c0c19154a1720a734d
Reggie Thompson <reggie.thompson4@gmail.com>
2021-05-19 20:11:43 -0600

    Add GET_SPOTIFY query to queries.js

--- src/queries.js
+++ src/queries.js
@@ -1,16 +1,25 @@
 import { gql } from '@apollo/client';
 
 export const GET_SINGLE_ALBUM = gql`
-query ($title: String!) {
-  album(title: $title) {
-    id
-    title
-    artists {
-      name
+  query ($title: String!) {
+    album(title: $title) {
+      id
+      title
+      artists {
+        name
+      }
+      year
+      genres
+      coverImage
+      uri
     }
-    year
-    genres
-    coverImage
   }
-}
+`
+
+export const GET_SPOTIFY = gql`
+query ($title: String!) {
+  spotifyAlbumId(title: $title) {
+      id
+    }
+  }
 `
