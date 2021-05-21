0c1cab69ad9e3b0c3715f79ae540e5a525edfc55
Reggie Thompson <reggie.thompson4@gmail.com>
2021-05-20 18:51:55 -0600

    Convert classes to BEM standard convention

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -7,7 +7,7 @@ import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 // import FavoriteIcon from '@material-ui/icons/Favorite';
 import { Tooltip } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
-import { useQuery, useApolloClient, gql } from '@apollo/client';
+import { useQuery, useApolloClient } from '@apollo/client';
 import { GET_SINGLE_ALBUM, GET_SPOTIFY } from '../../queries';
 
 
@@ -32,36 +32,40 @@ const DetailsModal = ({ title, id }) => {
 
   if (loading1) return <p>Loading...</p>; 
   if (error1) return <p>Error :(</p>;
-
+  if (loading2) return <p>Loading...</p>; 
+  if (error2) return <p>Error :(</p>;
+  
   return (
-    <div className="modal-container">
-      <section className="modal" data-cy="modal">
-        <article className="box left">
-          <img data-cy="album-cover" className="album-cover shadow" src={data1.album.coverImage} alt={`${data1.album.title} album cover`}/>
-          <div className="links">
+    <>
+      <section className="details_main" data-cy="modal">
+        <article className="details_box left">
+          <img data-cy="album-cover" className="details_album-cover shadow" src={data1.album.coverImage} alt={`${data1.album.title} album cover`}/>
+          <div className="details_links">
           <Tooltip title="Add to Favorites" placement="right">
-            <FavoriteBorderIcon data-cy="favorites-button" aria-label={"Add to Favorites"} className="favorite-button click"/>
+            <FavoriteBorderIcon data-cy="favorites-button" aria-label={"Add to Favorites"} className="details_favorite-button click"/>
           </Tooltip>
-            <div data-cy="" className="discogs-link-details">
-              <a data-cy="discogs-link" className="direct-link" href={data1.album.uri} target="_blnk"><img className="discogs-logo" src={discogsLogo} alt="discogs logo"/></a>
-            </div>
+            {/* <div data-cy="" className="details_discogs-link"> */}
+              <a data-cy="details_discogs-link" className="details_discogs-link" href={data1.album.uri} target="_blnk">
+                <img className="details_discogs-logo" src={discogsLogo} alt="discogs logo"/>
+              </a>
+            {/* </div> */}
           </div>
-          <div className="modal-text">
+          <div className="details_main-text">
             <p data-cy="artist-name">{data1.album.artists[0].name}</p>
             <p data-cy="album-title">{data1.album.title}</p>
             <p data-cy="release-year">Released: {data1.album.year}</p>
           </div>
         </article>
-        <article className="box right">
-          <iframe data-cy="web-player" title={`${data1.title} album album playlist`} className="shadow spotify-player" src={`https://open.spotify.com/embed/album/${data2.spotifyAlbumId.id}`} allowtransparency="true" allow="encrypted-media"></iframe>
+        <article className="details_box right">
+          <iframe data-cy="web-player" title={`${data1.title} album album playlist`} className="shadow details_spotify-player" src={`https://open.spotify.com/embed/album/${data2.spotifyAlbumId.id}`} allowtransparency="true" allow="encrypted-media"></iframe>
         </article>
       </section>
-      <div className="close-container">
+      {/* <div className="close-container">
         <Tooltip title="Close">
           <CloseIcon data-cy="close-button" aria-label={"close modal"} className="close-icon click"/>
         </Tooltip>
-      </div>
-    </div>
+      </div> */}
+    </>
   )
 }
 
