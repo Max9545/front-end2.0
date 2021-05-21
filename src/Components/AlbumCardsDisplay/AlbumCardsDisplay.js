53479444f771207afca8297a195c84f565d27389
Max Bregman <67295227+Max9545@users.noreply.github.com>
2021-05-21 17:17:01 -0600

    Tried to hook up random albums to display on page load, met with Back End once i figured out the call wasnt working

--- src/Components/AlbumCardsDisplay/AlbumCardsDisplay.js
+++ src/Components/AlbumCardsDisplay/AlbumCardsDisplay.js
@@ -5,17 +5,7 @@ import { useQuery } from '@apollo/client';
 
 function AlbumCardsDisplay ({ titles }) {
 
-  const { loading, error, data } = useQuery(GET_RANDOM_ALBUMS)
-
-  if (loading) return 'Loading..'
-  if (error) return 'Error! '
-
-  const [randomAlbums, setRandomeAlbums] = useState([])
-
-  useEffect(() => {
-    setRandomeAlbums(data)
-  })
-
+  
   const renderGivenAlbums = (titles) => {
     return titles.map(title => {
       return <AlbumCard title={ title } />
@@ -25,8 +15,7 @@ function AlbumCardsDisplay ({ titles }) {
   return (
     <div className='card-container' data-cy='card-container'>
       <h2>Albums</h2>
-        {!randomAlbums && renderGivenAlbums(titles) }
-        {randomAlbums && renderGivenAlbums(randomAlbums)}
+        {renderGivenAlbums(titles) }
     </div>
   )
 }
