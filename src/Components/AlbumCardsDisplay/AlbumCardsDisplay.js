164b847fc9485a7bf996645e038d0a314ce35d68
Max Bregman <67295227+Max9545@users.noreply.github.com>
2021-05-25 14:29:52 -0600

    Cleanup

--- src/Components/AlbumCardsDisplay/AlbumCardsDisplay.js
+++ src/Components/AlbumCardsDisplay/AlbumCardsDisplay.js
@@ -3,15 +3,21 @@ import AlbumCard from '../AlbumCard/AlbumCard';
 import { useQuery } from '@apollo/client';
 import { GET_ALBUMS_BY_ARTIST }from '../../queries';
 
-function AlbumCardsDisplay ({ titles }) {
-  
-  const renderGivenAlbums = (titles) => {
-    return titles.map(title => {
-      return (
-      <AlbumCard
-        title={ title }
-      />
-      )
+function AlbumCardsDisplay ({ artist }) {
+
+
+
+ const { loading, error, data } = useQuery(GET_ALBUMS_BY_ARTIST, {
+    variables: { artist: artist }
+  })
+
+  if (loading) return 'Loading...'
+  if (error) return `Error! ${error.message}`  
+
+
+  const renderGivenAlbums = (list) => {
+    return list.map(album => {
+      return <AlbumCard  artist={artist} album={album}/>
     })
   }
 
