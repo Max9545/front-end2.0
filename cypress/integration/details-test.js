391efbf4edc5f9ea16d7d4f4f25e46558b4983fa
Reggie Thompson <reggie.thompson4@gmail.com>
2021-05-24 21:57:27 -0600

    Update data-cy for testing

--- cypress/integration/details-test.js
+++ cypress/integration/details-test.js
@@ -11,43 +11,38 @@
 //   })
 
 //   it('should display the current album cover', () => {
-//     cy.get('[data-cy=album-cover')
+//     cy.get('[data-cy=details_album-cover')
 //       .should('have.attr', 'src', 'https://img.discogs.com/MUELn9ObTL-ZpxyUgF5M9D_Kumc=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-719877-1247870750.jpeg.jpg')
 //   });
 
 //   it('should allow users to add the current album to their collection of favorites', () => {
-//     cy.get('[data-cy=favorites-button')
+//     cy.get('[data-cy=details_favorites-button')
 //       .should('exist')
 //   });
 
 //   it('should allow users to visit discogs.com from the modal', () => {
-//     cy.get('[data-cy=discogs-link')
+//     cy.get('[data-cy=details_discogs-link')
 //       .should('exist')
 //   });
 
 //   it('should display the current album\'s artsit', () => {
-//     cy.get('[data-cy=artist-name')
+//     cy.get('[data-cy=details_artist-name')
 //       .contains('James Brown')
 //   });
 
 //   it('should display the current album title', () => {
-//     cy.get('[data-cy=album-title')
+//     cy.get('[data-cy=details_album-title')
 //       .contains('The Payback')
 //   });
 
 //   it('should display the year the current album was released', () => {
-//     cy.get('[data-cy=release-year')
+//     cy.get('[data-cy=details_release-year')
 //       .contains('Released: 1973')
 //   });
 
 //   it('should display a playlist where users can preview that album', () => {
-//     cy.get('[data-cy=web-player')
+//     cy.get('[data-cy=details_web-player')
 //       .should('have.attr', 'src')
 //       .should('include', 'https://open.spotify.com/embed/album/')
-//   });
-
-//   it('should allow users to close the modal window', () => {
-//     cy.get('[data-cy=close-button')
-//       .should('exist')
 //   });
 // })
