32777e8cc21bc813c2c5bb4e817128688027bdbd
Max Bregman <67295227+Max9545@users.noreply.github.com>
2021-05-26 11:30:44 -0600

    Make tests work by moving visit to be in one it block

--- cypress/integration/albumCards.js
+++ cypress/integration/albumCards.js
@@ -1,40 +1,46 @@
 context('Album Cards', () => {
+
+  
+
   beforeEach(() => {
     cy
-    
-    .intercept('POST', 'https://pure-hollows-05817.herokuapp.com/https://tranquil-depths-91575.herokuapp.com/graphql', (req) => {
-      if (req.body.query.includes('Miles Davis')) {
-        req.reply({ fixture: 'albumCards.js' })
+    .intercept('POST', 'https://tranquil-depths-91575.herokuapp.com/graphql', (req) => {
+      if (req.body.query.includes('John')) {
+        req.reply({ fixture: 'albumsCards.json' })
       }
     })
     // .visit('https://turing-selector.herokuapp.com/')
-    .visit('http://localhost:3000/')
   })
+
+  
 
   it('Should have an album cover', () => {
     cy
+    .visit('http://localhost:3000/')
+    .get('[data-cy=card_card]')
     .get('[data-cy=card_cover]').should('exist')
-    .should('have.attr', 'src', 'https://img.discogs.com/qM6vhS4-h_W6VJvo6xqM542yo80=/fit-in/600x597/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-13366963-1563275592-3337.jpeg.jpg')
+    .should('have.attr', 'src', 'https://img.discogs.com/S-ffrwNacsI2r9pt_O2t9aoSZqs=/fit-in/600x603/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-9896569-1621854039-3478.jpeg.jpg')
   })
 
   it('Should have an album card with a title', () => {
     cy
     .get('[data-cy=card_card]')
     .should('exist')
-    .get('[data-cy=card_title]').should('exist').should('contain', 'Miles Davis In A Blue Mood')
+    .get('[data-cy=card_title]').should('exist').should('contain', 'Mating Call ')
   })
 
   it('Should have an album artist name', () => {
     cy
+    .get('[data-cy=card_card]')
     .get('[data-cy=card_artist-container]')
     .should('exist')
-    .should('contain', 'Miles Davis')
+    .should('contain', 'John Coltrane')
   })
 
   it('Should have an album version release date', () => {
     cy
     .get('[data-cy=card_date]').should('exist')
-    .should('contain', 'This Edition Released in 1951')
+    .should('contain', 'This Edition Released in 1956')
   })
 
   it('Should have album fomat', () => {
@@ -45,6 +51,7 @@ context('Album Cards', () => {
 
   it('Should have album label', () => {
     cy
+    .get('[data-cy=card_card]')
     .get('[data-cy=card_genre]').should('exist')
     .should('contain', 'Prestige')
   })
