8b43c664e1886511abf0a1272edda607f2f00c27
Max Bregman <67295227+Max9545@users.noreply.github.com>
2021-05-26 10:45:12 -0600

    Write new card test

--- cypress/fixtures/albumsCards.js
+++ cypress/fixtures/albumsCards.js
@@ -1,94 +1,94 @@
 {
   "data": {
       "artistAlbums": [
-          {
-              "artist": "Led Zeppelin",
-              "title": "Led Zeppelin",
-              "id": 4126,
-              "coverImage": "https://img.discogs.com/12uE8RJXKZRG8G0AXQ5eVgzIQSk=/fit-in/600x597/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-3994862-1351723590-1086.jpeg.jpg",
-              "format": null,
-              "year": 1968,
-              "label": null
-          },
-          {
-              "artist": "Otis Redding / Led Zeppelin",
-              "title": "Free Me / Whole Lotta Love",
-              "id": 2625192,
-              "coverImage": "https://img.discogs.com/V4n1dD3kbmzME_ekElrbk5Q8uk4=/fit-in/397x397/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2625192-1293851493.jpeg.jpg",
-              "format": "7\", Jukebox",
-              "year": 1969,
-              "label": "Atlantic"
-          },
-          {
-              "artist": "Led Zeppelin / Peter, Paul And Mary*",
-              "title": "Whole Lotta Love / Leaving On A Jet Plane",
-              "id": 4685146,
-              "coverImage": "https://img.discogs.com/ZzLxblPJ3MBt5oTCjHqoSci2Gs0=/fit-in/567x567/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-4685146-1372175486-7021.jpeg.jpg",
-              "format": "7\", Single, Promo",
-              "year": 1969,
-              "label": "Hispavox"
-          },
-          {
-              "artist": "Dusty Springfield / Led Zeppelin",
-              "title": "Promotional LP For Record Department-In-Store-Play",
-              "id": 5508532,
-              "coverImage": "https://img.discogs.com/_fkGWtK6S2D21_4gscmo9w5Z_L0=/fit-in/600x606/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-5508532-1537396356-7710.jpeg.jpg",
-              "format": "LP, Promo, Smplr",
-              "year": 1969,
-              "label": "Atlantic"
-          },
-          {
-              "artist": "Led Zeppelin",
-              "title": "Good Times, Bad Times / Communication Breakdown / Dazed And Confused",
-              "id": 5662471,
-              "coverImage": "https://img.discogs.com/4dGBlTkvxwqYsHU6fc08DbndiTY=/fit-in/527x527/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-5662471-1545146097-6812.jpeg.jpg",
+            {
+              "artist": "Miles Davis",
+              "title": "Miles Davis In A Blue Mood",
+              "id": 13366963,
+              "coverImage": "https://img.discogs.com/qM6vhS4-h_W6VJvo6xqM542yo80=/fit-in/600x597/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-13366963-1563275592-3337.jpeg.jpg",
               "format": "7\", EP",
-              "year": 1969,
-              "label": "Atlantic"
+              "year": 1951,
+              "label": "Prestige"
           },
           {
-              "artist": "Led Zeppelin / Vanilla Fudge",
-              "title": "Led Zeppelin / Near The Beginning",
-              "id": 10709655,
-              "coverImage": "https://img.discogs.com/87cdaad65d702d6c6b1bb999daf248d0ea25b3e9/images/spacer.gif",
-              "format": "LP, Transcription",
-              "year": 1969,
-              "label": "Afrts"
-          },
-          {
-              "artist": "Led Zeppelin",
-              "title": "Led Zeppelin II",
-              "id": 4170,
-              "coverImage": "https://img.discogs.com/6MvLWXR88INzd9mqemT3hya3nWs=/fit-in/560x505/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-5691130-1400135575-1533.png.jpg",
+              "artist": "Fats Navarro - Leo Parker / Miles Davis - Charlie Parker",
+              "title": "Goin To Mintons / Half-Nelson",
+              "id": 1127273,
+              "coverImage": "https://img.discogs.com/6aCq8ghEsqfz6BCdlYPhjSjrqp0=/fit-in/600x597/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6229747-1476285472-7473.png.jpg",
               "format": null,
-              "year": 1969,
+              "year": 1947,
               "label": null
           },
           {
-              "artist": "Led Zeppelin",
-              "title": "Whole Lotta Love",
-              "id": 5249,
-              "coverImage": "https://img.discogs.com/3c7FjoMIUY5Iw0xHGpJer71YEaU=/fit-in/597x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-3894015-1348354436-4380.jpeg.jpg",
-              "format": null,
-              "year": 1969,
-              "label": null
-          },
-          {
-              "artist": "Led Zeppelin",
-              "title": "Babe I’m Gonna Leave You / Dazed & Confused",
-              "id": 679404,
-              "coverImage": "https://img.discogs.com/G9-XiL9gr-TWLgbFEPs-rmU3qtk=/fit-in/599x599/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-5622286-1415815232-7415.jpeg.jpg",
-              "format": null,
-              "year": 1969,
-              "label": null
-          },
-          {
-              "artist": "Led Zeppelin II*",
-              "title": "Ramble On",
-              "id": 679406,
+              "artist": "Charley Parker* With Miles Davis",
+              "title": "Buzzy/Donna Lee",
+              "id": 718653,
               "coverImage": null,
               "format": null,
-              "year": 1969,
+              "year": 1947,
+              "label": null
+          },
+          {
+              "artist": "Charlie Parker Quintet*  / Charlie Parker Accompanied By Miles Davis, Duke Jordan, Tommy Potter, Max Roach",
+              "title": "Bongo Bop / Embraceable You",
+              "id": 605520,
+              "coverImage": null,
+              "format": null,
+              "year": 1948,
+              "label": null
+          },
+          {
+              "artist": "Miles Davis",
+              "title": "Budo / Move",
+              "id": 1093821,
+              "coverImage": "https://img.discogs.com/MRkDBFPnHBKrFJWrIAMaom-JZKo=/fit-in/500x500/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-5341003-1390971787-2874.jpeg.jpg",
+              "format": null,
+              "year": 1949,
+              "label": null
+          },
+          {
+              "artist": "Lee Konitz Featuring Miles Davis",
+              "title": "The New Sounds",
+              "id": 3996004,
+              "coverImage": null,
+              "format": "10\", Mono",
+              "year": 1951,
+              "label": "Prestige"
+          }.
+          {
+              "artist": "Lee Konitz with Miles Davis",
+              "title": "Lee Konitz with Miles Davis",
+              "id": 13512697,
+              "coverImage": null,
+              "format": "7\", EP",
+              "year": 1951,
+              "label": "Prestige"
+          },
+          {
+              "artist": "Miles Davis",
+              "title": "The New Sounds",
+              "id": 1000170,
+              "coverImage": "https://img.discogs.com/qnQA33OBAzOAEhA3YsXykfGMsGM=/fit-in/600x611/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-7919266-1451670437-3704.jpeg.jpg",
+              "format": null,
+              "year": 1951,
+              "label": null
+          },
+          {
+              "artist": "Lee Konitz Featuring Miles Davis And  Billy Bauer",
+              "title": "Yesterdays / Duet For Saxophone And Guitar",
+              "id": 1487758,
+              "coverImage": null,
+              "format": null,
+              "year": 1951,
+              "label": null
+          },
+          {
+              "artist": "Fats Navarro / Dizzy Gillespie / Miles Davis / Kenny Dorham",
+              "title": "Modern Jazz Trumpets",
+              "id": 800621,
+              "coverImage": "https://img.discogs.com/G0Nd06yZ9IpzJmWvwnJc9iARfDA=/fit-in/500x500/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-3675042-1339878497-9205.jpeg.jpg",
+              "format": null,
+              "year": 1951,
               "label": null
           }
       ]
--- cypress/integration/albumCards.js
+++ cypress/integration/albumCards.js
@@ -1,54 +1,51 @@
 context('Album Cards', () => {
   beforeEach(() => {
     cy
-    .intercept('POST', 'https://tranquil-depths-91575.herokuapp.com/graphql', (req) => {
-      if (req.body.query.includes('The Payback')) {
-        req.reply({ fixture: 'details-fixture.json' })
-      }
-    })
-    .intercept('POST', 'https://tranquil-depths-91575.herokuapp.com/graphql', (req) => {
-      if (req.body.query.includes('Freak Out')) {
-        req.reply({ fixture: 'freakOut.json' })
-      }
-    })
-    .visit('http://localhost:3000/')
-    .get('.search__input')
-    .type('Freak Out')
     
-    cy.get('[data-cy=search-submit]')
-      .click()
+    .intercept('POST', 'https://pure-hollows-05817.herokuapp.com/https://tranquil-depths-91575.herokuapp.com/graphql', (req) => {
+      if (req.body.query.includes('Miles Davis')) {
+        req.reply({ fixture: 'albumCards.js' })
+      }
+    })
+    // .visit('https://turing-selector.herokuapp.com/')
+    .visit('http://localhost:3000/')
   })
 
   it('Should have an album cover', () => {
     cy
     .get('[data-cy=card_cover]').should('exist')
-    .should('have.attr', 'src', 'https://img.discogs.com/lcaNDTtBOojvbG2a5Fo8xdfzFXI=/fit-in/600x606/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-538636-1435321700-7614.jpeg.jpg')
+    .should('have.attr', 'src', 'https://img.discogs.com/qM6vhS4-h_W6VJvo6xqM542yo80=/fit-in/600x597/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-13366963-1563275592-3337.jpeg.jpg')
   })
 
   it('Should have an album card with a title', () => {
     cy
     .get('[data-cy=card_card]')
     .should('exist')
-    .get('[data-cy=card_title]').should('exist').should('contain', 'Freak Out')
+    .get('[data-cy=card_title]').should('exist').should('contain', 'Miles Davis In A Blue Mood')
   })
 
   it('Should have an album artist name', () => {
     cy
     .get('[data-cy=card_artist-container]')
     .should('exist')
-    .should('contain', 'The Mothers')
+    .should('contain', 'Miles Davis')
   })
 
   it('Should have an album version release date', () => {
     cy
     .get('[data-cy=card_date]').should('exist')
-    .should('contain', 'This Edition Released in 1966')
+    .should('contain', 'This Edition Released in 1951')
   })
 
-  it('Should have album genres', () => {
+  it('Should have album fomat', () => {
     cy
     .get('[data-cy=card_genre]').should('exist')
-    .should('contain', 'Electronic')
-    .should('contain', 'Rock')
+    .should('contain', '7\", EP')
+  })
+
+  it('Should have album label', () => {
+    cy
+    .get('[data-cy=card_genre]').should('exist')
+    .should('contain', 'Prestige')
   })
 })
