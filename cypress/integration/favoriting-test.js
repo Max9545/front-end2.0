9f2ed801a2f0490570b39f612d84bbd82a5263bb
Marika <marikashanahan@gmail.com>
2021-05-25 22:36:49 -0600

    Write toggling ability on favoriting-test.js

--- cypress/integration/favoriting-test.js
+++ cypress/integration/favoriting-test.js
@@ -4,17 +4,29 @@ describe('User can favorite or unfavorite any single album with the favoriting b
     cy.visit('http://localhost:3000/The%20Wall')
   })
   
-  it('Should have a favoriting button', () => {
+  it('Should have a favoriting button that starts out unfilled', () => {
     cy.get('[data-cy=details_favorites-button_unfilled]')
       .should('exist')
       .and('be.visible')
   })
 
-  it('User should be able to click on favoriting button', () => {
+  it('Favoriting button should render UI change from unfilled to filled icon onClick', () => {
     cy.get('[data-cy=details_favorites-button_unfilled]')
       .click()
     cy.get('[data-cy=details_favorites-button_filled]')
       .should('exist')
       .and('be.visible')
   })
+
+  it('Favoriting button should be able to toggle between unfilled and filled icons on multiple clicks', () => {
+    cy.get('[data-cy=details_favorites-button_unfilled]')
+      .click()
+    cy.get('[data-cy=details_favorites-button_filled]')
+      .click()
+    cy.get('[data-cy=details_favorites-button_unfilled]')
+      .should('exist')
+      .and('be.visible')
+  })
+
+  it('User should be able to see the same al')
 })
