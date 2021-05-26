f7316438319a1ca49a7369e2ab7acc4fa5de3ebb
Max Bregman <67295227+Max9545@users.noreply.github.com>
2021-05-26 10:31:46 -0600

    Add cards fixture

---
+++ cypress/fixtures/albumsCards.js
@@ -0,0 +1,96 @@
+{
+  "data": {
+      "artistAlbums": [
+          {
+              "artist": "Led Zeppelin",
+              "title": "Led Zeppelin",
+              "id": 4126,
+              "coverImage": "https://img.discogs.com/12uE8RJXKZRG8G0AXQ5eVgzIQSk=/fit-in/600x597/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-3994862-1351723590-1086.jpeg.jpg",
+              "format": null,
+              "year": 1968,
+              "label": null
+          },
+          {
+              "artist": "Otis Redding / Led Zeppelin",
+              "title": "Free Me / Whole Lotta Love",
+              "id": 2625192,
+              "coverImage": "https://img.discogs.com/V4n1dD3kbmzME_ekElrbk5Q8uk4=/fit-in/397x397/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2625192-1293851493.jpeg.jpg",
+              "format": "7\", Jukebox",
+              "year": 1969,
+              "label": "Atlantic"
+          },
+          {
+              "artist": "Led Zeppelin / Peter, Paul And Mary*",
+              "title": "Whole Lotta Love / Leaving On A Jet Plane",
+              "id": 4685146,
+              "coverImage": "https://img.discogs.com/ZzLxblPJ3MBt5oTCjHqoSci2Gs0=/fit-in/567x567/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-4685146-1372175486-7021.jpeg.jpg",
+              "format": "7\", Single, Promo",
+              "year": 1969,
+              "label": "Hispavox"
+          },
+          {
+              "artist": "Dusty Springfield / Led Zeppelin",
+              "title": "Promotional LP For Record Department-In-Store-Play",
+              "id": 5508532,
+              "coverImage": "https://img.discogs.com/_fkGWtK6S2D21_4gscmo9w5Z_L0=/fit-in/600x606/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-5508532-1537396356-7710.jpeg.jpg",
+              "format": "LP, Promo, Smplr",
+              "year": 1969,
+              "label": "Atlantic"
+          },
+          {
+              "artist": "Led Zeppelin",
+              "title": "Good Times, Bad Times / Communication Breakdown / Dazed And Confused",
+              "id": 5662471,
+              "coverImage": "https://img.discogs.com/4dGBlTkvxwqYsHU6fc08DbndiTY=/fit-in/527x527/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-5662471-1545146097-6812.jpeg.jpg",
+              "format": "7\", EP",
+              "year": 1969,
+              "label": "Atlantic"
+          },
+          {
+              "artist": "Led Zeppelin / Vanilla Fudge",
+              "title": "Led Zeppelin / Near The Beginning",
+              "id": 10709655,
+              "coverImage": "https://img.discogs.com/87cdaad65d702d6c6b1bb999daf248d0ea25b3e9/images/spacer.gif",
+              "format": "LP, Transcription",
+              "year": 1969,
+              "label": "Afrts"
+          },
+          {
+              "artist": "Led Zeppelin",
+              "title": "Led Zeppelin II",
+              "id": 4170,
+              "coverImage": "https://img.discogs.com/6MvLWXR88INzd9mqemT3hya3nWs=/fit-in/560x505/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-5691130-1400135575-1533.png.jpg",
+              "format": null,
+              "year": 1969,
+              "label": null
+          },
+          {
+              "artist": "Led Zeppelin",
+              "title": "Whole Lotta Love",
+              "id": 5249,
+              "coverImage": "https://img.discogs.com/3c7FjoMIUY5Iw0xHGpJer71YEaU=/fit-in/597x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-3894015-1348354436-4380.jpeg.jpg",
+              "format": null,
+              "year": 1969,
+              "label": null
+          },
+          {
+              "artist": "Led Zeppelin",
+              "title": "Babe I’m Gonna Leave You / Dazed & Confused",
+              "id": 679404,
+              "coverImage": "https://img.discogs.com/G9-XiL9gr-TWLgbFEPs-rmU3qtk=/fit-in/599x599/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-5622286-1415815232-7415.jpeg.jpg",
+              "format": null,
+              "year": 1969,
+              "label": null
+          },
+          {
+              "artist": "Led Zeppelin II*",
+              "title": "Ramble On",
+              "id": 679406,
+              "coverImage": null,
+              "format": null,
+              "year": 1969,
+              "label": null
+          }
+      ]
+  }
+}
