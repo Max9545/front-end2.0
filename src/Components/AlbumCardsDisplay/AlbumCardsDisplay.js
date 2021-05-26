f8e1ae6cb53ad324134038f6bddf8cf286b64b34
Max Bregman <67295227+Max9545@users.noreply.github.com>
2021-05-26 09:48:44 -0600

    Delete console logs

--- src/Components/AlbumCardsDisplay/AlbumCardsDisplay.js
+++ src/Components/AlbumCardsDisplay/AlbumCardsDisplay.js
@@ -3,18 +3,18 @@ import AlbumCard from '../AlbumCard/AlbumCard';
 
 function AlbumCardsDisplay ({ artist }) {
 
-console.log(artist)
+
 
  const { loading, error, data } = useQuery(GET_ALBUMS_BY_ARTIST, {
     variables: { artist: artist }
   })
-  console.log(data)
+
   if (loading) return 'Loading...'
   if (error) return `Error! ${error.message}`  
 
 
   const renderGivenAlbums = (list) => {
-    console.log(list)
+
     return list.map(album => {
       return <AlbumCard  album={album}/>
     })
