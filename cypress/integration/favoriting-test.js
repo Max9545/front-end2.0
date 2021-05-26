f3a91c3a9db7ad61a87dd46e31cb6fd087f51576
Marika <marikashanahan@gmail.com>
2021-05-26 00:02:47 -0600

    Apply intercept to favoriting-test.js

--- cypress/integration/favoriting-test.js
+++ cypress/integration/favoriting-test.js
@@ -1,7 +1,18 @@
 describe('User can favorite or unfavorite any single album with the favoriting button that will result in an accurate render on the /your-favorites route', () => {
   
   beforeEach(() => {
-    cy.visit('http://localhost:3000/The%20Wall')
+    cy
+      .intercept('POST', 'https://tranquil-depths-91575.herokuapp.com/graphql', (req) => {
+      if (req.body.query.includes('The Payback')) {
+        req.reply({ fixture: 'details-fixture.json' })
+      }
+    })
+      .intercept('POST', 'https://tranquil-depths-91575.herokuapp.com/graphql', (req) => {
+      if (req.body.query.includes('Freak Out')) {
+        req.reply({ fixture: 'freakOut.json' })
+      }
+    })
+    cy.visit('http://localhost:3000/The%20Payback')
   })
   
   it('Should have a favoriting button that starts out unfilled', () => {
@@ -28,5 +39,37 @@ describe('User can favorite or unfavorite any single album with the favoriting b
       .and('be.visible')
   })
 
-  // it('User should be able to see the same al')
+  it('User should be able to see the same album favorited rendered on the /your-favorites path', () => {
+    cy.get('[data-cy=details_favorites-button_unfilled]')
+      .click()
+    cy.get('[data-cy=link-to-liked]')
+      .click();
+    cy.get('[data-cy=card_cover]')
+      .should('have.attr', 'src', 'https://img.discogs.com/MUELn9ObTL-ZpxyUgF5M9D_Kumc=/fit-in/600x591/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-719877-1247870750.jpeg.jpg');
+    cy.get('[data-cy=card_title]')
+      .should('contain', 'The Payback');
+    cy.get('[data-cy=card_artist-container]')
+      .should('contain', 'James Brown');
+    cy.get('[data-cy=card_date]')
+      .should('contain', 'This Edition Released in 1973');
+    cy.get('[data-cy=card_genre]')
+      .should('contain', 'Funk')
+      .should('contain', 'Soul');
+  })
+
+  // Can implement test after expected branch is nmerged
+  // it('User should be able to favorite multiple albums and see them rendered on the /your-favorite path accurately', () => {
+  //   cy.get('[data-cy=details_favorites-button_unfilled]')
+  //     .click();
+  //   cy.get('[data-cy=search__input]').type('Freak Out');
+  //   cy.get('[data-cy=search-submit]').click();
+  //   cy.get('[data-cy=card_card]').click();
+  //   cy.get('[data-cy=details_favorites-button_unfilled]')
+  //     .click();
+  //   cy.get('[data-cy=link-to-liked]')
+  //     .click();
+  //   cy.get('[data-cy=card-container] > [data-cy=card_card]')
+  //     .get('[data-cy=card_title]')
+  //     .should('contain', 'Freak Out');
+  // })
 })
